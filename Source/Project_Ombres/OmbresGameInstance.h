846c62ea5b8738c1e0fe2a3df955bd635035647e
Tété <thophilecandelier@yahoo.fr>
2020-01-27 09:59:28 +0100

    * Module compiled and added to the project * Need to add a slate widget

---
+++ Source/Project_Ombres/OmbresGameInstance.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "OmbresGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT_OMBRES_API UOmbresGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void LoadComplete(const float LoadTime, const FString& MapName) override;
+	
+};
