cb44144cb489251d05d4bdf990615e144ab08d8a
Tété <thophilecandelier@yahoo.fr>
2020-02-11 18:08:53 +0100

    * Skywalk previews WIP

--- Source/Project_Ombres/SkywalkComponent.cpp
+++ Source/Project_Ombres/SkywalkComponent.cpp
@@ -10,6 +10,7 @@
 #include "Kismet/GameplayStatics.h"
 #include "Components/SkeletalMeshComponent.h"
 #include "EngineUtils.h"
+#include "Kismet/KismetMathLibrary.h"
 
 
 USkywalkComponent::USkywalkComponent()
@@ -30,7 +31,7 @@ USkywalkComponent::USkywalkComponent()
 	DistanceToGrabNewScraps = 125;
 	SpaceBetweenScraps = 100;
 	DistanceFromCamera2 = 1400;
-	BasePlatformAngle = 15;
+	BasePlatformAngle = 25;
 	TilesSpawnProbability = 0.9;
 	SpellEnabled = true;
 
@@ -96,9 +97,11 @@ void USkywalkComponent::TickComponent(float DeltaTime, ELevelTick TickType, FAct
 
 void USkywalkComponent::StartSkyWalk()
 {
-
 	if (SpellEnabled) {
 		if (!OnCoolDown) {
+			SortScrapsInWorld();
+			GeneratePropsTypes();
+
 			Active = true;
 			APlayerCameraManager* cameraManager = UGameplayStatics::GetPlayerCameraManager(GetWorld(), 0);
 			cameraManager->ViewPitchMin = -45;
@@ -123,9 +126,8 @@ void USkywalkComponent::StartSkyWalk()
 void USkywalkComponent::EndSkyWalk()
 {
 	if (Active) {
-		SpawnPlatform(ScrapFinalMiddlePosition + FVector(0, 0, -75));
-		SpawnPlatform(ScrapFinalMiddlePosition2 + FVector(0, 0, -75));
-
+		SpawnPlatform(ScrapFinalMiddlePosition + FVector(0, 0, -100));
+		SpawnPlatform(ScrapFinalMiddlePosition2 + FVector(0, 0, -120));
 
 		Player->GetCharacterMovement()->MaxWalkSpeed = 765;
 		APlayerCameraManager* cameraManager = UGameplayStatics::GetPlayerCameraManager(GetWorld(), 0);
@@ -178,13 +180,11 @@ void USkywalkComponent::UpdateSkywalk()
 	FVector cameraRightVector = (cameraManager->GetCameraRotation() + FRotator(0, 90, 0)).Vector()*(ScrapsPerLine%2==0?SpaceBetweenScraps/2:SpaceBetweenScraps);
 	FVector offsetVector = FVector(0, 0, -25);
 
-
 	ScrapFinalMiddlePosition2 = playerPosition + cameraForwardVector * 700 + offsetVector-cameraRightVector;
 	ScrapFinalMiddlePosition = playerPosition + cameraForwardVector * 350 + offsetVector - cameraRightVector;
 	ScrapMiddlePosition2 = cameraPosition + cameraForwardVector * DistanceFromCamera2- cameraRightVector;
 	ScrapMiddlePosition = cameraPosition + cameraForwardVector * DistanceFromCamera- cameraRightVector;
 	ScrapRightOffset = (cameraManager->GetCameraRotation() + FRotator(0, 90, 0)).Vector() * SpaceBetweenScraps;
-
 
 	if ((LastPlatformPosition - playerPosition).Size()>DistanceToGrabNewScraps) {
 		LastPlatformPosition = playerPosition;
@@ -193,8 +193,6 @@ void USkywalkComponent::UpdateSkywalk()
 		platform->SpawnFirstLine();
 		platform->SetDelay(0.1);	//Spawns second scrap line after a delay
 	}
-
-
 }
 
 ASkywalkPlatform* USkywalkComponent::SpawnPlatform(FVector Position)
@@ -226,32 +224,12 @@ void USkywalkComponent::ReleaseScraps()
 
 AActor* USkywalkComponent::GetClosestScrap()
 {
-	AActor* nearest = nullptr;
-
-	float minDist = TNumericLimits<float>::Max();
-
-	for (int i = 0, l = ScrapsInWorld.Num(); i < l; i++) {
-		AActor* actor = ScrapsInWorld[i];
-		if (actor == NULL) {
-			ScrapsInWorld.RemoveAt(i);
-			i--;
-			l--;
-			continue;
-		}
-
-		if (!ScrapsInUse.Contains(actor)) {
-			if (actor != NULL && Player != NULL) {
-				float distance = FVector::Dist(Player->GetActorLocation(), actor->GetActorLocation());
-
-				if (distance < minDist && distance < MaxRangeToGrabScrap) {
-					nearest = actor;
-					minDist = distance;
-				}
-			}
-			
-		}
+	if (ScrapsInWorld.Num() > 0) {
+		return ScrapsInWorld[0];
 	}
-	return nearest;
+	else {
+		return nullptr;
+	}
 }
 
 
@@ -262,5 +240,59 @@ void USkywalkComponent::FinishSkywalk()
 	EndSkyWalk();
 }
 
+void USkywalkComponent::GeneratePropsTypes()
+{
+	propsTypes.Empty();
+	propsTypes.SetNum(100);
+
+	for (int i = 0; i < 100; i++) {
+		propsTypes[i] = FMath::RandRange(0, 7);
+	}
+}
 
 
+void USkywalkComponent::SortScrapsInWorld()
+{
+	TArray<FScrapWithDistance> scrapsWithDistance;
+	FVector playerLocation = Player->GetActorLocation();
+
+	for (int i =0,l=ScrapsInWorld.Num();i<l;i++){
+		AActor* Actor = ScrapsInWorld[i];
+		FScrapWithDistance scrapWithDistance;
+		scrapWithDistance.scrap = Actor;
+		scrapWithDistance.distance = UKismetMathLibrary::Vector_Distance(Actor->GetActorLocation(), playerLocation);
+		scrapsWithDistance.Add(scrapWithDistance);
+	}
+
+	TArray<FScrapWithDistance> sortedScraps;
+
+	int index = 0;
+	int scrapsNum = scrapsWithDistance.Num();
+	while (index < scrapsNum) {
+		FScrapWithDistance scrapWithDist = scrapsWithDistance[index];
+		for (int i = 0, l = scrapsWithDistance.Num(); i < l; i++) {
+			if (i >= index) {
+				sortedScraps.Add(scrapWithDist);
+				index++;
+				break;
+			}
+			else {
+				if (scrapWithDist.distance > MaxRangeToGrabScrap) {
+					index++;
+					break;
+				}
+				if (scrapWithDist.distance < sortedScraps[i].distance) {
+					sortedScraps.Insert(scrapWithDist, i);
+					index++;
+					break;
+				}
+			}
+		}
+	}
+
+	ScrapsInWorld.SetNum(scrapsNum);
+	for (int i = 0; i < scrapsNum; i++) {
+		ScrapsInWorld[i] = sortedScraps[i].scrap;
+		UE_LOG(LogTemp, Warning, TEXT("scrap distance : %f"), sortedScraps[i].distance);
+	}
+}
