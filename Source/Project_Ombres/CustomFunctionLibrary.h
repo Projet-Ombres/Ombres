6ce682837995af03f1b2ed1e3e2bfba08b2f6f5a
Théophile Candelier <thophilecandelier@yahoo.fr>
2020-09-05 18:33:54 +0200

    * Removed unused steam functions

--- Source/Project_Ombres/CustomFunctionLibrary.h
+++ Source/Project_Ombres/CustomFunctionLibrary.h
@@ -48,10 +48,6 @@ class PROJECT_OMBRES_API UCustomFunctionLibrary : public UBlueprintFunctionLibra
 public:
 
 	UFUNCTION(BlueprintCallable)
-		static void ResetSteamAchievement(FString achievementID);
-
-
-	UFUNCTION(BlueprintCallable)
 		static void SetCollisionResponseToChannels(UPrimitiveComponent* Component, FCollisionResponseContainer collision);
 
 	UFUNCTION(BlueprintCallable)
